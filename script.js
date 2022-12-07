7e03347cef66d1ce50217f3c50605a23cc8ba689
MarinaPrivalova <privalovama@yandex.ru>
2022-12-07 17:52:07 +0300

    fix: profile

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const buttonShow = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const buttonClose = document.querySelector('.popup__close-button');
+
+const userName = document.querySelector('.profile__name');
+const userJob = document.querySelector('.profile__vocation');
+
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#vocation');
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
-    
 }
-
 buttonShow.addEventListener('click', openPopup); 
-
-сonsole.log('test') 
-
-
-
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+buttonClose.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+    closePopup();
+}
+formElement.addEventListener('submit', handleFormSubmit);    
