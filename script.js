8e9caf409e0b1b841cd0f3677f6a11affa9e3427
MarinaPrivalova <privalovama@yandex.ru>
2022-12-07 19:22:50 +0300

    feat: JS, fix: photo-grid

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const buttonShow = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const buttonClose = document.querySelector('.popup__close-button');
-
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__vocation');
-
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('#name');
+const nameInput = document.querySelector('#username');
 const jobInput = document.querySelector('#vocation');
-
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
 }
-buttonShow.addEventListener('click', openPopup); 
 
 function closePopup() {
     popup.classList.remove('popup_opened');
+    nameInput.value = '';
+    jobInput.value = '';
 }
-buttonClose.addEventListener('click', closePopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
-    closePopup();
+    return closePopup();
 }
-formElement.addEventListener('submit', handleFormSubmit);    
+
+buttonShow.addEventListener('click', openPopup); 
+buttonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
