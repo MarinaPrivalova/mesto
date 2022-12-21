9bf585f7d2766ec40e6a9fbeed4c9730f05132fa
MarinaPrivalova <privalovama@yandex.ru>
2022-12-21 10:05:47 +0300

    feat:JS:добавить карточки, открыть/закрыть попап, лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,108 @@
-const buttonShow = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
+const initialCards = [
+    {
+      name: 'Алтай',
+      link: 'https://images.unsplash.com/photo-1598394188724-cdeb618eb4e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80'
+    },
+    {
+      name: 'Озеро Байкал',
+      link: 'https://images.unsplash.com/photo-1548130729-90d4d11826f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Карачаево-Черкессия',
+      link: 'https://images.unsplash.com/photo-1627327719562-f1f61e8364fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+      name: 'Ладожское озеро',
+      link: 'https://images.unsplash.com/photo-1632212383828-78d78ea41d8d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80'
+    },
+    {
+      name: 'Сихотэ-Алинь',
+      link: 'https://images.unsplash.com/photo-1644543419167-2cc7a5738665?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      name: 'Сулакский каньон',
+      link: 'https://images.unsplash.com/photo-1598535348425-e76a7cc312d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    }
+  ]; // 6 готовых карточек
+const buttonOpenEditProfile = document.querySelector('.profile__edit-button'); //кнопка редактирования профиля
+const buttonOpenAddNewCard = document.querySelector('.profile__add-button'); //кнопка добавления карточки
 const buttonClose = document.querySelector('.popup__close-button');
-const userName = document.querySelector('.profile__name');
-const userJob = document.querySelector('.profile__vocation');
-const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('#username');
-const jobInput = document.querySelector('#vocation');
 
-function openPopup() {
+/*const buttonCloseEditProfile = document.querySelector('.popup__close-button'); //кнопка закрытия попапа редактирования профиля
+const buttonCloseAddNewCard = document.querySelector('.popup__close-button'); //кнопка закрытия попапа добавления карточки*/
+
+const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit'); //попап редактирования профиля
+const popupAddNewCard = document.querySelector('.popup_type_new-card'); //попап добаления карточки
+const userName = document.querySelector('.profile__name'); //имя профиля
+const userJob = document.querySelector('.profile__vocation'); //профессия профиля
+const cardName = document.querySelector ('.card__title'); //название карточки
+const cardPhoto = document.querySelector ('.card__photo'); //фотография карточки
+const formElement = document.querySelector('.popup__form'); //форма попапа
+const nameInput = document.querySelector('.popup__input_type_user-name'); //инпут редактирования имени профиля
+const jobInput = document.querySelector('.popup__input_type_vocation'); //инпут редактирования профессии профиля
+const cardNameInput = document.querySelector('.popup__input_type_image-name'); //инпут добавления названия карточки
+const cardLinkInput = document.querySelector('.popup__input_type_image-link'); //инпут добавления ссылки на фото
+const buttonLike = document.querySelector('.card__button-like'); //кнопка лайк
+const cardGalegy =document.querySelector('.photo__list'); //галерея карточек
+
+//открыть попап
+function openPopup(popup) {
     popup.classList.add('popup_opened');
+};
+
+//закрыть попап
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+//загрузить данные из профиля
+function popupLoadData() {
     nameInput.value = userName.textContent;
     jobInput.value = userJob.textContent;
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-    nameInput.value = '';
-    jobInput.value = '';
+//удалить несохранённые данные
+function deleteUnsavedData() {
+  nameInput.value = '';
+  jobInput.value = '';
 }
 
+//сохранить данные
 function handleFormSubmit (evt) {
     evt.preventDefault();
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
-    return closePopup();
+    closePopup(popup)
 }
 
-buttonShow.addEventListener('click', openPopup); 
-buttonClose.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+//лайкнуть фото
+function likePhoto() {
+    buttonLike.classList.toggle('.card__button-like_active');
+}
+
+//добавить карточки
+const getCard = initialCards.forEach(function getCardElement(item) {
+    const cardTemplate = document.querySelector('#card').content; //получить содержимое template
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true); //клонировать элементы карточки
+    cardElement.querySelector('.card__photo').src = item.link; //ссылка из массива initialCards
+    cardElement.querySelector('.card__photo').alt = item.name; //имя из массива initialCards
+    cardElement.querySelector('.card__title').textContent = item.name; //имя из массива initialCards
+    cardGalegy.append(cardElement); //добавление элементов в список
+  }); 
+
+buttonLike.addEventListener('click', likePhoto);
+
+buttonOpenEditProfile.addEventListener('click', () => {openPopup(popupEditProfile); popupLoadData()});
+buttonClose.addEventListener('click', () => {closePopup(popupEditProfile); deleteUnsavedData()});
+formElement.addEventListener('submit', handleFormSubmit, closePopup(popupEditProfile));
+
+buttonOpenAddNewCard.addEventListener('click', () => {openPopup(popupAddNewCard);});
+buttonClose.addEventListener('click', () => {closePopup(popupAddNewCard);});
+
+
+
+
+/*formElement.addEventListener('submit', handleFormSubmit, closePopup(popupAddNewCard));*/
+/*cardName.textContent = cardNameInput.value;
+    cardPhoto.textContent = cardLinkInput.value;*/
