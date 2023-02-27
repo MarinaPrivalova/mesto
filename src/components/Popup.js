3285a7f640d46c85c13bd21c9522e56584d43b32
MarinaPrivalova <privalovama@yandex.ru>
2023-02-27 07:23:48 +0300

    fix close

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,25 +1,32 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
-    this._handleEscAndOverlayClose = this._handleEscAndOverlayClose.bind(this);
+    this._handleEscClose = this._handleEscClose.bind(this);
+    this._overlayClose = this._overlayClose.bind(this);
   }
 
-  _handleEscAndOverlayClose(evt) {
-    if (evt.key === 'Escape' || evt.target == evt.currentTarget) {
+  _handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      this.close()
+    }
+  }
+
+  _overlayClose(evt) {
+    if (evt.target == evt.currentTarget) {
       this.close()
     }
   }
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscAndOverlayClose);
-    this._popup.addEventListener('mousedown', this._handleEscAndOverlayClose);
+    document.addEventListener('keydown', this._handleEscClose);
+    this._popup.addEventListener('mousedown', this._overlayClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscAndOverlayClose);
-    this._popup.removeEventListener('mousedown', this._handleEscAndOverlayClose);
+    document.removeEventListener('keydown', this._handleEscClose);
+    this._popup.removeEventListener('mousedown', this._overlayClose);
   }
 
   setEventListeners() {
